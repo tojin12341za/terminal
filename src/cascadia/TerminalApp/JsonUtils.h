06d72312f4b737249d2d353aa8cc95e0d21cdb0d
Dustin L. Howett <duhowett@microsoft.com>
2020-05-12 23:12:50 -0700

    Delete JsonUtils and replace it with JsonUtilsNew

--- src/cascadia/TerminalApp/JsonUtils.h
+++ src/cascadia/TerminalApp/JsonUtils.h
@@ -9,136 +9,358 @@ Abstract:
 - Helpers for the TerminalApp project
 Author(s):
 - Mike Griese - August 2019
+- Dustin Howett - January 2020
 
 --*/
 #pragma once
 
+#include <json.h>
+
+#include "../types/inc/utils.hpp"
+
+namespace winrt
+{
+    // If we don't use winrt, nobody will include the ConversionTrait for winrt::guid.
+    // If nobody includes it, this forward declaration will suffice.
+    struct guid;
+}
+
 namespace TerminalApp::JsonUtils
 {
-    void GetOptionalColor(const Json::Value& json,
-                          std::string_view key,
-                          std::optional<til::color>& target);
+    namespace Detail
+    {
+        // Function Description:
+        // - Returns a string_view to a Json::Value's internal string storage,
+        //   hopefully without copying it.
+        __declspec(noinline) inline const std::string_view GetStringView(const Json::Value& json)
+        {
+            const char* begin{ nullptr };
+            const char* end{ nullptr };
+            json.getString(&begin, &end);
+            const std::string_view zeroCopyString{ begin, gsl::narrow_cast<size_t>(end - begin) };
+            return zeroCopyString;
+        }
 
-    void GetOptionalString(const Json::Value& json,
-                           std::string_view key,
-                           std::optional<std::wstring>& target);
+        template<typename T>
+        struct DeduceOptional
+        {
+            using Type = T;
+        };
 
-    void GetOptionalGuid(const Json::Value& json,
-                         std::string_view key,
-                         std::optional<GUID>& target);
+        template<typename TOpt>
+        struct DeduceOptional<std::optional<TOpt>>
+        {
+            using Type = TOpt;
+        };
+    }
 
-    void GetOptionalDouble(const Json::Value& json,
-                           std::string_view key,
-                           std::optional<double>& target);
+    class TypeMismatchException : public std::runtime_error
+    {
+    public:
+        TypeMismatchException() :
+            runtime_error("invalid type") {}
+    };
+
+    class KeyedException : public std::runtime_error
+    {
+    public:
+        KeyedException(const std::string_view key, const std::exception& exception) :
+            runtime_error(fmt::format("error parsing \"{0}\": {1}", key, exception.what()).c_str()) {}
+    };
+
+    template<typename T>
+    struct ConversionTrait
+    {
+        // FromJson, CanConvert are not defined so as to cause a compile error (which forces a specialization)
+    };
+
+    template<>
+    struct ConversionTrait<std::string>
+    {
+        std::string FromJson(const Json::Value& json)
+        {
+            return json.asString();
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isString();
+        }
+    };
+
+    template<>
+    struct ConversionTrait<std::wstring>
+    {
+        std::wstring FromJson(const Json::Value& json)
+        {
+            return til::u8u16(Detail::GetStringView(json));
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isString();
+        }
+    };
+
+    template<>
+    struct ConversionTrait<bool>
+    {
+        bool FromJson(const Json::Value& json)
+        {
+            return json.asBool();
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isBool();
+        }
+    };
+
+    template<>
+    struct ConversionTrait<int>
+    {
+        int FromJson(const Json::Value& json)
+        {
+            return json.asInt();
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isInt();
+        }
+    };
+
+    template<>
+    struct ConversionTrait<unsigned int>
+    {
+        unsigned int FromJson(const Json::Value& json)
+        {
+            return json.asUInt();
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isUInt();
+        }
+    };
+
+    template<>
+    struct ConversionTrait<float>
+    {
+        float FromJson(const Json::Value& json)
+        {
+            return json.asFloat();
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isNumeric();
+        }
+    };
+
+    template<>
+    struct ConversionTrait<double>
+    {
+        double FromJson(const Json::Value& json)
+        {
+            return json.asDouble();
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isNumeric();
+        }
+    };
+
+    template<>
+    struct ConversionTrait<GUID>
+    {
+        GUID FromJson(const Json::Value& json)
+        {
+            return ::Microsoft::Console::Utils::GuidFromString(Detail::GetStringView(json));
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isString();
+        }
+    };
+
+    // (GUID and winrt::guid are mutually convertible!)
+    template<>
+    struct ConversionTrait<winrt::guid> : public ConversionTrait<GUID>
+    {
+    };
+
+    template<>
+    struct ConversionTrait<til::color>
+    {
+        til::color FromJson(const Json::Value& json)
+        {
+            return ::Microsoft::Console::Utils::ColorFromHexString(Detail::GetStringView(json));
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isString();
+        }
+    };
+
+    template<typename T, typename TBase>
+    struct EnumMapper
+    {
+        using pair_type = std::pair<std::string_view, T>;
+        T FromJson(const Json::Value& json)
+        {
+            const auto name{ Detail::GetStringView(json) };
+            for (const auto& pair : TBase::mappings)
+            {
+                if (pair.first == name)
+                {
+                    return pair.second;
+                }
+            }
+            // the first mapping is the "Default"
+            return TBase::mappings[0].second;
+        }
+
+        bool CanConvert(const Json::Value& json)
+        {
+            return json.isString();
+        }
+    };
 
     // Method Description:
-    // - Helper that can be used for retrieving an optional value from a json
-    //   object, and parsing it's value to layer on a given target object.
-    //    - If the key we're looking for _doesn't_ exist in the json object,
-    //      we'll leave the target object unmodified.
-    //    - If the key exists in the json object, but is set to `null`, then
+    // - Helper that will populate a reference with a value converted from a json object.
+    // Arguments:
+    // - json: the json object to convert
+    // - target: the value to populate with the converted result
+    // Return Value:
+    // - a boolean indicating whether the value existed (in this case, was non-null)
+    //
+    // GetValue, type-deduced, manual converter
+    template<typename T, typename Converter>
+    bool GetValue(const Json::Value& json, T& target, Converter&& conv)
+    {
+        if (json)
+        {
+            if (!conv.CanConvert(json))
+            {
+                throw TypeMismatchException{};
+            }
+
+            target = conv.FromJson(json);
+            return true;
+        }
+        return false;
+    }
+
+    // Method Description:
+    // - Overload on GetValue that will populate a std::optional with a value converted from json
+    //    - If the json value doesn't exist we'll leave the target object unmodified.
+    //    - If the json object is set to `null`, then
     //      we'll instead set the target back to nullopt.
-    // - Each caller should provide a conversion function that takes a
-    //   Json::Value and returns an object of the same type as target.
     // Arguments:
-    // - json: The json object to search for the given key
-    // - key: The key to look for in the json object
-    // - target: the optional object to receive the value from json
-    // - conversion: a std::function<T(const Json::Value&)> which can be used to
-    //   convert the Json::Value to the appropriate type.
-    // - validation: optional, if provided, will be called first to ensure that
-    //   the json::value is of the correct type before attempting to call
-    //   `conversion`.
+    // - json: the json object to convert
+    // - target: the value to populate with the converted result
     // Return Value:
-    // - <none>
-    template<typename T, typename F>
-    void GetOptionalValue(const Json::Value& json,
-                          std::string_view key,
-                          std::optional<T>& target,
-                          F&& conversion,
-                          const std::function<bool(const Json::Value&)>& validation = nullptr)
+    // - a boolean indicating whether the optional was changed
+    //
+    // GetValue, type-deduced for optional, manual converter
+    template<typename TOpt, typename Converter>
+    bool GetValue(const Json::Value& json, std::optional<TOpt>& target, Converter&& conv)
     {
-        if (json.isMember(JsonKey(key)))
+        if (json.isNull())
         {
-            if (auto jsonVal{ json[JsonKey(key)] })
-            {
-                if (validation == nullptr || validation(jsonVal))
-                {
-                    target = conversion(jsonVal);
-                }
-            }
-            else
-            {
-                // This branch is hit when the json object contained the key,
-                // but the key was set to `null`. In this case, explicitly clear
-                // the target.
-                target = std::nullopt;
-            }
+            target = std::nullopt;
+            return true; // null is valid for optionals
         }
+
+        TOpt local{};
+        if (GetValue(json, local, std::forward<Converter>(conv)))
+        {
+            target = std::move(local);
+            return true;
+        }
+        return false;
     }
 
-    // Method Description:
-    // - Helper that can be used for retrieving a value from a json
-    //   object, and parsing it's value to set on a given target object.
-    //    - If the key we're looking for _doesn't_ exist in the json object,
-    //      we'll leave the target object unmodified.
-    //    - If the key exists in the json object, we'll use the provided
-    //      `validation` function to ensure that the json value is of the
-    //      correct type.
-    //    - If we successfully validate the json value type (or no validation
-    //      function was provided), then we'll use `conversion` to parse the
-    //      value and place the result into `target`
-    // - Each caller should provide a conversion function that takes a
-    //   Json::Value and returns an object of the same type as target.
-    // - Unlike GetOptionalValue, if the key exists but is set to `null`, we'll
-    //   just ignore it.
-    // Arguments:
-    // - json: The json object to search for the given key
-    // - key: The key to look for in the json object
-    // - target: the optional object to receive the value from json
-    // - conversion: a std::function<T(const Json::Value&)> which can be used to
-    //   convert the Json::Value to the appropriate type.
-    // - validation: optional, if provided, will be called first to ensure that
-    //   the json::value is of the correct type before attempting to call
-    //   `conversion`.
-    // Return Value:
-    // - <none>
-    template<typename T, typename F>
-    void GetValue(const Json::Value& json,
-                  std::string_view key,
-                  T& target,
-                  F&& conversion,
-                  const std::function<bool(const Json::Value&)>& validation = nullptr)
+    // GetValue, forced return type, manual converter
+    template<typename T, typename Converter>
+    T GetValue(const Json::Value& json, Converter&& conv)
     {
-        if (json.isMember(JsonKey(key)))
-        {
-            if (auto jsonVal{ json[JsonKey(key)] })
-            {
-                if (validation == nullptr || validation(jsonVal))
-                {
-                    target = conversion(jsonVal);
-                }
-            }
-        }
+        T local{};
+        GetValue(json, local, std::forward<Converter>(conv));
+        return local; // returns zero-initialized or value
     }
 
-    void GetInt(const Json::Value& json,
-                std::string_view key,
-                int& target);
+    // GetValueForKey, type-deduced, manual converter
+    template<typename T, typename Converter>
+    bool GetValueForKey(const Json::Value& json, std::string_view key, T& target, Converter&& conv)
+    {
+        if (auto found{ json.find(&*key.cbegin(), (&*key.cbegin()) + key.size()) })
+        {
+            try
+            {
+                return GetValue(*found, target, std::forward<Converter>(conv));
+            }
+            catch (const std::exception& e)
+            {
+                // WRAP! WRAP LIKE YOUR LIFE DEPENDS ON IT!
+                throw KeyedException(key, e);
+            }
+        }
+        return false;
+    }
 
-    void GetUInt(const Json::Value& json,
-                 std::string_view key,
-                 uint32_t& target);
+    // GetValueForKey, forced return type, manual converter
+    template<typename T, typename Converter>
+    T GetValueForKey(const Json::Value& json, std::string_view key, Converter&& conv)
+    {
+        T local{};
+        GetValueForKey(json, key, local, std::forward<Converter>(conv));
+        return local; // returns zero-initialized?
+    }
 
-    void GetDouble(const Json::Value& json,
-                   std::string_view key,
-                   double& target);
+    // GetValue, type-deduced, with automatic converter
+    template<typename T>
+    bool GetValue(const Json::Value& json, T& target)
+    {
+        return GetValue(json, target, ConversionTrait<typename Detail::DeduceOptional<T>::Type>{});
+    }
 
-    void GetBool(const Json::Value& json,
-                 std::string_view key,
-                 bool& target);
+    // GetValue, forced return type, with automatic converter
+    template<typename T>
+    T GetValue(const Json::Value& json)
+    {
+        T local{};
+        GetValue(json, local, ConversionTrait<typename Detail::DeduceOptional<T>::Type>{});
+        return local; // returns zero-initialized or value
+    }
 
-    void GetWstring(const Json::Value& json,
-                    std::string_view key,
-                    std::wstring& target);
+    // GetValueForKey, type-deduced, with automatic converter
+    template<typename T>
+    bool GetValueForKey(const Json::Value& json, std::string_view key, T& target)
+    {
+        return GetValueForKey(json, key, target, ConversionTrait<typename Detail::DeduceOptional<T>::Type>{});
+    }
+
+    // GetValueForKey, forced return type, with automatic converter
+    template<typename T>
+    T GetValueForKey(const Json::Value& json, std::string_view key)
+    {
+        return GetValueForKey<T>(json, key, ConversionTrait<typename Detail::DeduceOptional<T>::Type>{});
+    }
+
+    // Get multiple values for keys (json, k, &v, k, &v, k, &v, ...).
+    // Uses the default converter for each v.
+    // Careful: this can cause a template explosion.
+    constexpr void GetValuesForKeys(const Json::Value& /*json*/) {}
+
+    template<typename T, typename... Args>
+    void GetValuesForKeys(const Json::Value& json, std::string_view key1, T&& val1, Args&&... args)
+    {
+        GetValueForKey(json, key1, val1);
+        GetValuesForKeys(json, std::forward<Args>(args)...);
+    }
 };
